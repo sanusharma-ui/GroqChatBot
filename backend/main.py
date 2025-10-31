c61c9687b162eaf07d5fb79d923e8c7b625fa09b
Sanu00 <sanusharma0045.v@gmail.com>
2025-10-31 13:33:45 +0530

    hghs

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,8 @@ app.add_middleware(
         "http://localhost:8000",
         "http://127.0.0.1:5500",
         "http://127.0.0.1:8000",
-        "*",  # for Render or testing
+        "https://sonu-frontend.onrender.com/",
+        "*"  # for Render or testing
     ],
     allow_credentials=True,
     allow_methods=["*"],
